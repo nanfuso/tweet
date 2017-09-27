ffeca09e3e5cf743ae740e85f73a92695a2a5b40
natail <nataleanfuso@gmail.com>
2017-09-26 20:23:11 -0500

    validations to posts and users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
+
+    validates :text, presence: true
+    validates :text, length: { in: 1..140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts
+
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :user_name, presence: true
+    validates :user_name, uniqueness: true
+    validates :password, presence: true
+    validates :password, length: { in: 6..20}
 end
